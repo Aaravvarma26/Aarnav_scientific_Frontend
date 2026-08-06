fcc5c3cbed38cd5b3dcc477d3a214eb23644f9a2
aarav varma <aaravvarma2004@gmail.com>
2026-08-06 10:29:06 +0530

    Delete frontend directory

--- frontend/src/components/common/skeleton.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from "@/common/utils";
-
-export function Skeleton({ className }: { className?: string }) {
-  return <div className={cn("animate-pulse rounded-xl bg-navy-100", className)} />;
-}
