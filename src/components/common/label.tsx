989d2188705a5711beb88d501e3abea3c6f3891e
aarav varma <aaravvarma2004@gmail.com>
2026-08-09 02:02:50 +0530

    Delete src directory

--- src/components/common/label.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "@/common/utils";
-
-export function Label({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) {
-  return (
-    <label className={cn("mb-1.5 block text-sm font-medium text-navy-800", className)} {...props} />
-  );
-}
